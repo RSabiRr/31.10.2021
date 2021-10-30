c5b29a6e78ffc3368fdb98881e8dc965cecc0d34
sabir <tu201806274@code.edu.az>
2021-10-31 02:43:50 +0400

    ayliq gelir

---
+++ ConsoleApp5/ConsoleApp5/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Employee
+    {
+        public string name;
+        public string surname;
+        public string position;
+        public int dailSalary;
+
+        public string monthlySalary()
+        {
+            int ayliq;
+
+            ayliq = dailSalary * 30;
+
+            return ($"{name} {surname}    Ayliq geliri: {ayliq}AZN");
+
+
+        }
+
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.name = "Nurlan";
+            employee.surname = "Qasimov";
+            employee.dailSalary = 5;
+
+            Console.WriteLine(employee.monthlySalary());
+        }
+    }
+}
