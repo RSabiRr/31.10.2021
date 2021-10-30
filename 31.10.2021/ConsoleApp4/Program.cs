24df044cb7b8ed4300f2c362d35186795bb703ed
sabir <tu201806274@code.edu.az>
2021-10-31 01:48:46 +0400

    ayri siniflerde

---
+++ 31.10.2021/ConsoleApp4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task 1,2,3
+            //Console.WriteLine("****************************************");
+            //Console.WriteLine(" ");
+            //Console.WriteLine("CAR ABOUT: ");
+            //Console.WriteLine(" ");
+            //Car car1 = new Car();
+
+            //car1.model = "2106";
+            //car1.brand = "VAZ";
+            //car1.fuelCapacity = 40;
+
+            //Console.WriteLine(car1.about());
+            //Console.WriteLine(" ");
+            //Console.WriteLine(car1.way());
+            #endregion
+
+            #region task 4
+            //Console.WriteLine(" ");
+            //Console.WriteLine("***************************************");
+            //Console.WriteLine(" ");
+            //Console.WriteLine("BICYRCLE ABOUT: ");
+            //Console.WriteLine(" ");
+
+            //Bicyrcle bicyrcle = new Bicyrcle();
+
+            //bicyrcle.brand = "BMX";
+            //bicyrcle.model = "Kink Liberty";
+
+            //bicyrcle.about();
+            #endregion
+
+
+
+
+
+
+        }
+
+    }
+
+    class carbicyrcle
+    {
+
+
+
+    }
+}
